1085957c1cc865e40d32da8bb63a4d5b52a129de
Saveen Reddy <saveenr@live.com>
2017-09-03 13:01:12 -0700

    simplified script

--- Archive-DailyJobInformation.ps1
+++ Archive-DailyJobInformation.ps1
@@ -1,33 +1,54 @@
 Set-StrictMode -Version 2
 $ErrorActionPreference = "Stop"
 
-function get-daterange( [datetime] $start, [datetime] $end )
+class AdlJobDateRange
 {
-    $curdate = $staRT
-    while ($curdate -le $end)
+    [datetime] $Lower
+    [datetime] $Upper
+    [System.TimeSpan] $TimeSpan 
+
+    # Constructor
+    AdlJobDateRange([datetime] $start, [datetime] $end)
+    {
+        $this.Lower = $start
+        $this.Upper = $end
+        $this.TimeSpan = $end - $start
+    }
+}
+
+function __getdaterange( [AdlJobDateRange] $daterange )
+{
+    $curdate = $daterange.Lower
+    while ($curdate -le $daterange.Upper)
     {
         $curdate
         $curdate = $curdate.AddDays(1)        
     }
 }
 
-function get-dailyfilename( [string] $account, [string] $folder, [datetime] $date )
+function __getdailyfilename( [string] $account, [string] $folder, [datetime] $date )
 {
     $date = $date.Date
     $filename = Join-Path $folder ( "jobs_" + $account + "_" + $date.ToString("yyyyMMdd") + ".clixml")
     $filename
 }
 
-function Export-AdlJobHistory( [string] $account, [datetime] $start, [datetime] $end, [string] $folder, [bool] $overwrite=$false)
+function Get-AdlJobDateRange( [datetime] $start, [datetime] $end )
 {
-    $dates = get-daterange $startdate $enddate
+    $r = [AdlJobDateRange]::new( $start, $end )
+    $r
+}
+
+function Export-AdlJobHistory( [string] $account, [AdlJobDateRange] $daterange, [string] $folder, [bool] $overwrite=$false)
+{
+    $dates = __getdaterange $daterange
 
     foreach ($date in $dates)
     {
         $after = $date
         $before = $date.AddDays(1)
         $top = 10000
-        $filename = get-dailyfilename $account $folder $date
+        $filename = __getdailyfilename $account $folder $date
         Write-Host $date " => " $filename
 
         if ( Test-Path $filename )
@@ -41,7 +62,9 @@ function Export-AdlJobHistory( [string] $account, [datetime] $start, [datetime] 
 
         if ( !(Test-Path $filename) )
         {
-            $jobs = Get-AdlJob -Account $account -SubmittedAfter $after -SubmittedBefore $before -top $top
+            # Fetch the jobs - for analysis only get the jobs that have ended
+
+            $jobs = Get-AdlJob -Account $account -SubmittedAfter $after -SubmittedBefore $before -top $top -State Ended
             if ($jobs -ne $null)
             {
                 Write-Host "Num jobs" = $jobs.Count
@@ -53,47 +76,44 @@ function Export-AdlJobHistory( [string] $account, [datetime] $start, [datetime] 
 }
 
 
-
-function Import-AdlJobHistory( [string] $account, [datetime] $start, [datetime] $end, [string] $folder)
+function Import-AdlJobHistory( [string] $account, [AdlJobDateRange] $daterange, [string] $folder)
 {
-    $items = New-Object System.Collections.Generic.List[System.Object]
+    $dates = __getdaterange $daterange
 
-    $dates = get-daterange $startdate $enddate
     foreach ($date in $dates)
     {
         
-        $filename = get-dailyfilename $account $folder $date
+        $filename = __getdailyfilename $account $folder $date
 
         Write-Host $date " => " $filename
 
         if ( Test-Path $filename )
         {
             $jobs = Import-Clixml $filename
-            $items.AddRange($jobs)
+            foreach ($job in $jobs)
+            {
+                $job
+            }
         }
 
     }
-
-    $items
-
 }
 
 
-# $startdate = get-date 8/1/2017
-# $enddate = get-date 8/31/2017
+# $startdate = get-date 2017/8/16
+# $enddate = get-date 2017/8/20
+# $daterange = Get-AdlJobDateRange $startdate $enddate
 # $adla_accountname = "datainsights"
 # $output_folder = "D:\jobhistory"
 
 
 # Save daily job history
-# Export-AdlJobHistory -account $adla_accountname -start $startdate -end $enddate -folder $output_folder -overwrite $false
+# Export-AdlJobHistory -account $adla_accountname -daterange $daterange -folder $output_folder -overwrite $false
 
 
-# $jobs = Import-AdlJobHistory -account $adla_accountname -start $startdate -end $enddate -folder $output_folder 
-
-# $jobs | ConvertTo-Json | Out-file d:\jobhistory.json
-
+# Load the job back in
+# $jobs = Import-AdlJobHistory -account $adla_accountname -daterange $daterange -folder $output_folder 
 
 
-# select the properties you want
+# Select the properties you want
 # $jobs | Select Submitter,ErrorMessage,DegreeOfParallelism,Priority,SubmitTime,StartTime,EndTime,State,Result | ConvertTo-Json | Out-file d:\jobhistory2.json            
